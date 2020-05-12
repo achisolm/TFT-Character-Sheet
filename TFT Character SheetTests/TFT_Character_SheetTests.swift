bc2595dcd68df2e08de8dd46da272a381362adb6
achisolm <adachis@ut.utm.edu>
2020-05-12 09:15:12 -0500

    Initial Commit

---
+++ TFT Character SheetTests/TFT_Character_SheetTests.swift
@@ -0,0 +1,34 @@
+//
+//  TFT_Character_SheetTests.swift
+//  TFT Character SheetTests
+//
+//  Created by Adam Chisolm on 5/12/20.
+//  Copyright © 2020 Adam Chisolm. All rights reserved.
+//
+
+import XCTest
+@testable import TFT_Character_Sheet
+
+class TFT_Character_SheetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
